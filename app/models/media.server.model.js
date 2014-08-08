a21dfd1d16998b5ee31c4d0a982904e35939ff38
Brennon Bortz <brennon@brennonbortz.com>
2014-08-08 10:39:26 -0400

    Add other fields to media schema

--- app/models/media.server.model.js
+++ app/models/media.server.model.js
@@ -25,7 +25,18 @@ var MediaSchema = new Schema({
   label: {
     type: String,
     required: true
-  }
+  },
+  has_lyrics: Boolean,
+  comments: String,
+  year: Date,
+  excerpt_start_time: Number,
+  excerpt_end_time: Number,
+  bpm: Number,
+  source: String,
+  key: String,
+  emotion_tags: [Schema.Types.ObjectId],
+  genres: [Schema.Types.ObjectId],
+  file: Schema.Types.ObjectId
 });
 
 mongoose.model('Media', MediaSchema);
