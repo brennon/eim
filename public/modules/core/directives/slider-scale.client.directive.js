5da5423ce3d0bd510273adfbc2d81a0e2ab811e5
Brennon Bortz <brennon@brennonbortz.com>
2014-08-13 15:14:34 -0400

    Build slider scale directive

---
+++ public/modules/core/directives/slider-scale.client.directive.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Display Trial Data for debugging
+angular.module('core').directive('sliderScale', function() {
+  return {
+    restrict: 'E',
+    template: function(element, attrs) {
+
+      var htmlString = '';
+
+      switch (attrs.imageType) {
+        case 'single':
+          htmlString = '<div class="row"><div class="col-md-12 slider-image text-center"><img src="' + attrs.singleImageSrc + '" /></div><div class="col-md-2 text-center slider-image"></div><!----><div class="col-md-8 slider-image"><slider ng-model="' + attrs.sliderNgModel + '" floor="' + attrs.sliderFloor + '" ceiling="' + attrs.sliderCeiling + '" step="' + attrs.sliderStep + '" stretch="' + attrs.sliderStretch + '" ng-init="' + attrs.sliderNgModel + '=' + attrs.sliderInitialValue + '"></slider></div><!----><div class="col-md-2 text-center slider-image"></div></div>';
+          return htmlString;
+        case 'extremes':
+          htmlString = '<div class="row"><div class="col-md-2 text-center slider-image"></div><div class="col-md-2 slider-image text-center"><img src="' + attrs.leftImageSrc + '" /></div><!----><div class="col-md-4 slider-image"></div><!----><div class="col-md-2 slider-image text-center"><img src="' + attrs.rightImageSrc + '" /></div><!----><div class="col-md-2 text-center slider-image"></div><div class="col-md-2 text-center slider-image"></div><!----><div class="col-md-8 slider-image"><slider ng-model="' + attrs.sliderNgModel + '" floor="' + attrs.sliderFloor + '" ceiling="' + attrs.sliderCeiling + '" step="' + attrs.sliderStep + '" stretch="' + attrs.sliderStretch + '" ng-init="' + attrs.sliderNgModel + '=' + attrs.sliderInitialValue + '"></slider></div><!----><div class="col-md-2 text-center slider-image"></div></div>';
+          return htmlString;
+        default:
+          return htmlString;
+      }
+    }
+  }
+});
