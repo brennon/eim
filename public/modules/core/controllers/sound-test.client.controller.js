dbee25d81e34743250521e63ba4d9b9fb861bfd5
Brennon Bortz <brennon@brennonbortz.com>
2014-08-14 09:13:32 -0400

    Start and stop sound test from SoundTestController

--- public/modules/core/controllers/sound-test.client.controller.js
+++ public/modules/core/controllers/sound-test.client.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
-angular.module('core').controller('SoundTestController', ['$scope',
-  function($scope) {
+angular.module('core').controller('SoundTestController', ['$scope', '$http',
+  function($scope, $http) {
+//    $scope.trialDataJson = function() {
+//      return TrialData.toJson();
+//    }
+
+    function startSoundTest() {
+      $http.get('/api/osc/send/startSoundTest');
+    };
+
+    startSoundTest();
+
+    $scope.stopSoundTest = function() {
+      console.log('***** stopping sound test');
+      $http.get('/api/osc/send/stopSoundTest');
+    };
   }
 ]);
