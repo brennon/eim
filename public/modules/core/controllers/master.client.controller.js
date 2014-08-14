d78fcecc99db8e357c65417e3bb51a36ab5f781d
Brennon Bortz <brennon@brennonbortz.com>
2014-08-14 09:13:02 -0400

    Reset experiment on StartController, not MasterController

--- public/modules/core/controllers/master.client.controller.js
+++ public/modules/core/controllers/master.client.controller.js
@@ -1,15 +1,9 @@
 'use strict';
 
-angular.module('core').controller('MasterController', ['$scope', '$http',
-  function($scope, $http) {
-
-    // When the MasterController is loaded, get a new experiment setup
-    $http.get('/api/experiment-schemas/random')
-      .success(function(data) {
-        $scope.experimentSchema = data;
-      })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
+angular.module('core').controller('MasterController', ['$scope', 'TrialData',
+  function($scope, TrialData) {
+    $scope.trialDataJson = function() {
+      return TrialData.toJson();
+    };
   }
 ]);
--- public/modules/core/controllers/start.client.controller.js
+++ public/modules/core/controllers/start.client.controller.js
@@ -1,7 +1,21 @@
 'use strict';
 
-angular.module('core').controller('StartController', ['$scope',
-  function($scope) {
+angular.module('core').controller('StartController', ['$scope', '$http', 'TrialData',
+  function($scope, $http, TrialData) {
 
+    TrialData.reset();
+
+    // When the StartController is loaded, get a new experiment setup
+    $http.get('/api/experiment-schemas/random')
+      .success(function(data) {
+        $scope.experimentSchema = data;
+
+        for (var i in data.media) {
+          TrialData.data.media.push(data.media[i].label);
+        }
+      })
+      .error(function(data) {
+        console.error('Error: ' + data);
+      });
   }
 ]);
