2d4a3e08c89c69695e67b9e8049b05f6c2821c9b
Brennon Bortz <brennon@brennonbortz.com>
2014-08-14 09:12:24 -0400

    Reset functionality for TrialData

--- public/modules/core/services/trial-data.client.service.js
+++ public/modules/core/services/trial-data.client.service.js
@@ -2,18 +2,93 @@
 
 // Trial Data service used to persist data for individual trials
 angular.module('core').factory('TrialData', [
-  function() {
+  function () {
+
+    function blankDataObject() {
+      return {
+        answers: {
+          musical_expertise: null,
+          musical_background: null,
+          most_engaged: null,
+          hearing_impairements: null,
+          visual_impairements: null,
+          nationality: null,
+          dob: null,
+          sex: null,
+          music_styles: [],
+          most_enjoyed: null,
+          emotion_indices: [],
+          ratings: {
+            positivity: [],
+            like_dislike: [],
+            familiarity: [],
+            engagement: [],
+            activity: [],
+            chills: [],
+            power: []
+          }
+        },
+        date: null,
+        media: [],
+        timestamps: {
+          start: null,
+          test: null,
+          media: [],
+          end: null
+        },
+        metadata: {
+          session_number: null,
+          location: 'taipei_city',
+          terminal: null
+        }
+      };
+    }
+
     var trialData = {
       data: {
         answers: {
+          musical_expertise: null,
+          musical_background: null,
+          most_engaged: null,
+          hearing_impairements: null,
+          visual_impairements: null,
           nationality: null,
-          birthyear: null,
-          gender: null
+          dob: null,
+          sex: null,
+          music_styles: [],
+          most_enjoyed: null,
+          emotion_indices: [],
+          ratings: {
+            positivity: [],
+            like_dislike: [],
+            familiarity: [],
+            engagement: [],
+            activity: [],
+            chills: [],
+            power: []
+          }
         },
-        metadata: {}
+        date: null,
+        media: [],
+        timestamps: {
+          start: null,
+          test: null,
+          media: [],
+          end: null
+        },
+        metadata: {
+          session_number: null,
+          location: 'taipei_city',
+          terminal: null
+        }
       },
-      toJson: function() {
+
+      toJson: function () {
         return angular.toJson(this.data, true);
+      },
+
+      reset: function () {
+        this.data = blankDataObject();
       }
     };
 
