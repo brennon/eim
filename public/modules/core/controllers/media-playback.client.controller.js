2bdc523addb4ac2ecf663864feed8df0e5388280
Brennon Bortz <brennon@brennonbortz.com>
2014-09-15 17:00:18 -0400

    Add MediaPlayback controller

--- public/modules/core/controllers/media-playback.client.controller.js
+++ public/modules/core/controllers/media-playback.client.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('core').controller('Control MediaController', ['$scope', 'TrialData',
+angular.module('core').controller('MediaPlaybackController', ['$scope', 'TrialData',
   function($scope, TrialData) {
 
     /* global io */
@@ -10,19 +10,10 @@ angular.module('core').controller('Control MediaController', ['$scope', 'TrialDa
       console.log('socket "oscMessageSent" event received with data: ' + data);
     });
 
-    socket.emit('sendOSCMessage', {
-      oscType: 'message',
-      address: '/eim/control/startControlPlayback',
-      args: {
-        type: 'string',
-        value: TrialData.data.metadata.session_number
-      }
-    });
-
-    $scope.stopSoundTest = function() {
+    $scope.startMediaPlayback = function() {
       socket.emit('sendOSCMessage', {
         oscType: 'message',
-        address: '/eim/control/stopSoundTest',
+        address: '/eim/control/startMediaPlayback',
         args: {
           type: 'string',
           value: TrialData.data.metadata.session_number
