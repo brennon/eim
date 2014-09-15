671e4457fc24b052b4002eb2d7f80018ce5106c5
Brennon Bortz <brennon@brennonbortz.com>
2014-09-15 14:47:19 -0400

    Wire musical background responses to TrialData

--- public/modules/core/controllers/musical-background.client.controller.js
+++ public/modules/core/controllers/musical-background.client.controller.js
@@ -6,15 +6,6 @@ angular.module('core').controller('DemographicsController', ['$scope', 'TrialDat
     // Expose TrialData on scope
     $scope.trialData = TrialData;
 
-    // Value lists for form
-    $scope.nationalities = ['Afghan', 'Albanian', 'Algerian', 'American', 'Andorran', 'Angolan', 'Antiguans', 'Argentinean', 'Armenian', 'Australian', 'Austrian', 'Azerbaijani', 'Bahamian', 'Bahraini', 'Bangladeshi', 'Barbadian', 'Barbudans', 'Batswana', 'Belarusian', 'Belgian', 'Belizean', 'Beninese', 'Bhutanese', 'Bolivian', 'Bosnian', 'Brazilian', 'British', 'Bruneian', 'Bulgarian', 'Burkinabe', 'Burmese', 'Burundian', 'Cambodian', 'Cameroonian', 'Canadian', 'Cape Verdean', 'Central African', 'Chadian', 'Chilean', 'Chinese', 'Colombian', 'Comoran', 'Congolese', 'Costa Rican', 'Croatian', 'Cuban', 'Cypriot', 'Czech', 'Danish', 'Djibouti', 'Dominican', 'Dutch', 'East Timorese', 'Ecuadorean', 'Egyptian', 'Emirian', 'Equatorial Guinean', 'Eritrean', 'Estonian', 'Ethiopian', 'Fijian', 'Filipino', 'Finnish', 'French', 'Gabonese', 'Gambian', 'Georgian', 'German', 'Ghanaian', 'Greek', 'Grenadian', 'Guatemalan', 'Guinea-Bissauan', 'Guinean', 'Guyanese', 'Haitian', 'Herzegovinian', 'Honduran', 'Hungarian', 'I-Kiribati', 'Icelander', 'Indian', 'Indonesian', 'Iranian', 'Iraqi', 'Irish', 'Israeli', 'Italian', 'Ivorian', 'Jamaican', 'Japanese', 'Jordanian', 'Kazakhstani', 'Kenyan', 'Kittian and Nevisian', 'Kuwaiti', 'Kyrgyz', 'Laotian', 'Latvian', 'Lebanese', 'Liberian', 'Libyan', 'Liechtensteiner', 'Lithuanian', 'Luxembourger', 'Macedonian', 'Malagasy', 'Malawian', 'Malaysian', 'Maldivan', 'Malian', 'Maltese', 'Marshallese', 'Mauritanian', 'Mauritian', 'Mexican', 'Micronesian', 'Moldovan', 'Monacan', 'Mongolian', 'Moroccan', 'Mosotho', 'Motswana', 'Mozambican', 'Namibian', 'Nauruan', 'Nepalese', 'New Zealander', 'Nicaraguan', 'Nigerian', 'Nigerien', 'North Korean', 'Northern Irish', 'Norwegian', 'Omani', 'Pakistani', 'Palauan', 'Panamanian', 'Papua New Guinean', 'Paraguayan', 'Peruvian', 'Polish', 'Portuguese', 'Qatari', 'Romanian', 'Russian', 'Rwandan', 'SaintLucian', 'Salvadoran', 'Samoan', 'San Marinese', 'Sao Tomean', 'Saudi', 'Scottish', 'Senegalese', 'Serbian', 'Seychellois', 'Sierra Leonean', 'Singaporean', 'Slovakian', 'Slovenian', 'Solomon Islander', 'Somali', 'South African', 'South Korean', 'Spanish', 'Sri Lankan', 'Sudanese', 'Surinamer', 'Swazi', 'Swedish', 'Swiss', 'Syrian', 'Taiwanese', 'Tajik', 'Tanzanian', 'Thai', 'Togolese', 'Tongan', 'Trinidadian or Tobagonian', 'Tunisian', 'Turkish', 'Tuvaluan', 'Ugandan', 'Ukrainian', 'Uruguayan', 'Uzbekistani', 'Venezuelan', 'Vietnamese', 'Welsh', 'Yemenite', 'Zambian', 'Zimbabwean'];
-
-    $scope.years = [];
-    var thisYear = new Date().getFullYear();
-    for (var i = thisYear - 110; i <= thisYear; i++) {
-      $scope.years.push(i);
-    }
-
     // Save data to Trial Data
 
     // Downcase nationality for persisting
@@ -28,6 +19,33 @@ angular.module('core').controller('DemographicsController', ['$scope', 'TrialDat
 
     $scope.genderChanged = function(gender) {
       TrialData.data.answers.gender = gender;
+    };
+
+    $scope.musicianChanged = function(isMusician) {
+      TrialData.data.answers.musical_background = isMusician.toLowerCase() === "true" ? true : false;
+    };
+
+    $scope.hearingImpairmentsChanged = function(hasHearingImpairments) {
+      TrialData.data.answers.hearing_impairments = hasHearingImpairments.toLowerCase() === "true" ? true : false;
+    };
+
+    $scope.visualImpairmentsChanged = function(hasVisualImpairments) {
+      TrialData.data.answers.visual_impairments = hasVisualImpairments.toLowerCase() === "true" ? true : false;
+    };
+
+    $scope.$watch('musicalExpertise', function musicalExpertiseChanged(musicalExpertise) {
+      TrialData.data.answers.musical_expertise = musicalExpertise;
+    });
+
+    $scope.stylesChanged = function() {
+      var newStyles = [];
+      for (var prop in $scope.subject.styles) {
+        var style = prop.toString();
+        if ($scope.subject.styles[style]) {
+          newStyles.push(style);
+        }
+      }
+      TrialData.data.answers.music_styles = newStyles.sort();
     };
 
     $scope.trialDataJson = function() {
