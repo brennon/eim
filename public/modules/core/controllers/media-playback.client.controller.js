dd2be5a50918bccbe8968d9ae445d4c119a75a12
Brennon Bortz <brennon@brennonbortz.com>
2014-09-19 10:18:44 -0400

    Remove extraneous logging from MediaPlayback controller

--- public/modules/core/controllers/media-playback.client.controller.js
+++ public/modules/core/controllers/media-playback.client.controller.js
@@ -11,15 +11,9 @@ angular.module('core').controller('MediaPlaybackController', ['$scope', 'TrialDa
     /* global io */
     var socket = io();
 
+    // State to control button behavior
     $scope.currentButtonLabel = 'Begin Playback';
     $scope.mediaHasPlayed = false;
-
-    // Log incoming OSC messages
-    function oscMessageSentListener(data) {
-      console.log('media-playback.client.controller.js: oscMessageSent');
-      console.log('socket "oscMessageSent" event received with data: ' + data);
-    }
-    socket.on('oscMessageSent', oscMessageSentListener);
 
     // Send media playback message to Max
     $scope.startMediaPlayback = function () {
@@ -33,7 +27,7 @@ angular.module('core').controller('MediaPlaybackController', ['$scope', 'TrialDa
         args: [
           {
             type: 'string',
-            value: mediaLabel
+            value: '' + mediaLabel
           },
           {
             type: 'string',
@@ -50,18 +44,62 @@ angular.module('core').controller('MediaPlaybackController', ['$scope', 'TrialDa
     // Determine next state and send the user there
     $scope.gotoNextState = function () {
       if (!$scope.mediaHasPlayed) {
-        console.log('media has not played');
         $scope.startMediaPlayback();
       } else if (TrialData.data.state.mediaPlayCount >= TrialData.data.media.length) {
-        console.log('all media have played');
         $state.go('home');
       } else if (TrialData.data.state.mediaPlayCount !== TrialData.data.media.length) {
-        console.log('more media remain');
         $state.go('media-playback', {}, {reload: true});
       }
     };
 
+    // Setup listener for incoming OSC messages
+    function oscMessageReceivedListener(data) {
+
+      console.log('got message');
+
+      // If it was a media playback message
+      if (data.address === '/eim/status/playback') {
+
+        console.log('got playback message');
+
+        // If it was a start message
+        if (parseInt(data.args[0].value) === 1) {
+          console.log('got start message');
+
+          // Fade out screen
+          $scope.hideBody();
+
+          // Otherwise, if it was a stop message
+        } else if (parseInt(data.args[0].value) === 0) {
+
+          console.log('got stop message');
+
+          // Fade in screen
+          $scope.showBody();
+
+          if (!$scope.mediaHasPlayed) {
+
+            // Increment media play count
+            TrialData.data.state.mediaPlayCount++;
+
+            // Update state
+            $scope.$apply(function() {
+              $scope.currentButtonLabel = 'Continue';
+              $scope.mediaHasPlayed = true;
+            });
+          }
+        }
+      }
+
+      console.log('currentButtonLabel: ' + $scope.currentButtonLabel);
+      console.log('mediaHasPlayed: ' + $scope.mediaHasPlayed);
+    }
+
+    // Attach OSC message received listener
+    socket.on('oscMessageReceived', oscMessageReceivedListener);
+
     // Send a message to stop media playback when this controller is destroyed
+    // Also, remove OSC message event listeners
     $scope.$on('$destroy', function stopMediaPlayback() {
       socket.emit('sendOSCMessage', {
         oscType: 'message',
@@ -73,47 +111,6 @@ angular.module('core').controller('MediaPlaybackController', ['$scope', 'TrialDa
       });
 
       socket.removeListener('oscMessageReceived', oscMessageReceivedListener);
-      socket.removeListener('oscMessageSent', oscMessageSentListener);
     });
-
-    /**
-     * Incoming Max messages
-     */
-
-    // Setup listener for incoming OSC messages
-    function oscMessageReceivedListener(data) {
-      console.log('media-playback.client.controller.js: oscMessageReceived');
-      // If it was a media playback message
-      if (data.address === '/eim/status/playback') {
-
-        console.log('was playback message');
-
-        // If it was a start message
-        if (parseInt(data.args[0].value) === 1) {
-
-          console.log('was start');
-
-          // Fade out screen
-          $scope.hideBody();
-
-          // Otherwise, if it was a stop message
-        } else if (parseInt(data.args[0].value) === 0) {
-
-          console.log('was stop');
-
-          // Fade in screen
-          $scope.showBody();
-
-          if (!$scope.mediaHasPlayed) {
-            // Increment media play count
-            TrialData.data.state.mediaPlayCount++;
-
-            $scope.currentButtoneLabel = 'Continue';
-            $scope.mediaHasPlayed = true;
-          }
-        }
-      }
-    }
-    socket.on('oscMessageReceived', oscMessageReceivedListener);
   }
 ]);
