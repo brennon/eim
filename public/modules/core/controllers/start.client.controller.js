5068df5af9c5e94423eab2df5b7cf2ead91d1318
Brennon Bortz <brennon@brennonbortz.com>
2014-10-13 09:45:31 -0400

    Fetch ExperimentSchema through ExperimentManager

--- public/modules/core/controllers/start.client.controller.js
+++ public/modules/core/controllers/start.client.controller.js
@@ -1,12 +1,16 @@
 'use strict';
 
-angular.module('core').controller('StartController', ['$scope', '$http', '$timeout', 'TrialData',
-  function($scope, $http, $timeout, TrialData) {
+angular.module('core').controller('StartController', ['$scope', '$http', '$timeout', 'TrialData', 'ExperimentManager',
+  function ($scope, $http, $timeout, TrialData, ExperimentManager) {
+
+    // Setup socket
+    /* global io */
+    var socket = io();
 
     // Are all pieces of experiment ready to begin?
     $scope.maxReady = false;
     $scope.backendReady = false;
-    $scope.readyToAdvance = function() {
+    $scope.readyToAdvance = function () {
       if ($scope.debugMode) {
         return true;
       } else {
@@ -14,25 +18,25 @@ angular.module('core').controller('StartController', ['$scope', '$http', '$timeo
       }
     };
 
-    // Reset TrialData
-    TrialData.reset();
-
-    // Setup socket
-    /* global io */
-    var socket = io();
-
-    // Generate new session identifier and store it in TrialData
-    /* global UUID */
-    var sessionID = UUID.generate();
-    TrialData.data.metadata.session_number = sessionID;
+    // Reset experiment manager for new trial
+    ExperimentManager.masterReset().then(
+      function experimentResetSuccessHandler () {
+        $scope.backendReady = true;
+        sendExperimentStartMessage();
+      },
+      function experimentResetErrorHandler () {
+        $scope.addGenericErrorAlert();
+        throw new Error('An experiment schema could not be fetched from the server');
+      }
+    );
 
     // Configure handler for incoming OSC messages
-    var oscMessageReceivedListener = function(data) {
+    var oscMessageReceivedListener = function (data) {
       console.log('start.client.controller.js: oscMessageReceived');
 
       // If we received the resetComplete message
       if (data.address === '/eim/status/experimentReady') {
-        $scope.$apply(function() {
+        $scope.$apply(function () {
           $scope.maxReady = true;
         });
       }
@@ -46,11 +50,7 @@ angular.module('core').controller('StartController', ['$scope', '$http', '$timeo
       socket.removeListener('oscMessageReceived', oscMessageReceivedListener);
     });
 
-    // Get a new experiment setup from the backend
-    $http.get('/api/experiment-schemas/random')
-      .success(function(data) {
-
-        // Send experiment start message
+    var sendExperimentStartMessage = function () {
         socket.emit('sendOSCMessage', {
           oscType: 'message',
           address: '/eim/control/startExperiment',
@@ -59,27 +59,10 @@ angular.module('core').controller('StartController', ['$scope', '$http', '$timeo
             value: '' + TrialData.data.metadata.session_number
           }
         });
-
-        // Add received media to TrialData
-        for (var i in data.media) {
-          TrialData.data.media.push(data.media[i].label);
-        }
-
-        // Only proceed if we've got some media!
-        if (data.media.length > 0) {
-            $scope.backendReady = true;
-        } else {
-          $scope.addGenericErrorAlert();
-          throw new Error('No media was found in the fetched experiment schema');
-        }
-      })
-      .error(function(data) {
-        $scope.addGenericErrorAlert();
-        throw new Error('An experiment schema could not be fetched from the server');
-      });
+    };
 
     // If we aren't ready to go after 10 seconds, throw an error
-    $timeout(function() {
+    $timeout(function () {
       if (!$scope.readyToAdvance()) {
         $scope.addGenericErrorAlert();
         throw new Error('The experiment was not setup after 10 seconds');
