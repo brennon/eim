4a073ce0e240aea0ed488215b309f5f11e5cfbaa
Brennon Bortz <brennon@brennonbortz.com>
2014-10-13 09:28:34 -0400

    Only update TrialData when a path and value are supplied

--- public/modules/core/services/trial-data.client.service.js
+++ public/modules/core/services/trial-data.client.service.js
@@ -98,16 +98,18 @@ angular.module('core').factory('TrialData', [
       },
 
       setValueForPath: function(path, value) {
-        var schema = this;  // a moving reference to internal objects within this (this TrialData)
-        var pList = path.split('.');
-        var len = pList.length;
-        for(var i = 0; i < len-1; i++) {
-          var elem = pList[i];
-          if( !schema[elem] ) schema[elem] = {}
-          schema = schema[elem];
-        }
+        if (path && value) {
+          var schema = this;  // a moving reference to internal objects within this (this TrialData)
+          var pList = path.split('.');
+          var len = pList.length;
+          for (var i = 0; i < len - 1; i++) {
+            var elem = pList[i];
+            if (!schema[elem]) schema[elem] = {}
+            schema = schema[elem];
+          }
 
-        schema[pList[len-1]] = value;
+          schema[pList[len - 1]] = value;
+        }
       },
 
       setValueForPathForCurrentMedia: function(path, value) {
