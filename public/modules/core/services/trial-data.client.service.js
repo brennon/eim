a3f1d73fdf7a3f3e53f1c24a9570f7c212f20417
Brennon Bortz <brennon@brennonbortz.com>
2014-10-23 12:51:12 -0400

    TrialData tests

--- public/modules/core/services/trial-data.client.service.js
+++ public/modules/core/services/trial-data.client.service.js
@@ -2,146 +2,100 @@
 
 // Trial Data service used to persist data for individual trials
 angular.module('core').factory('TrialData', [
-  function () {
+    function() {
 
-    function blankDataObject() {
-      return {
-        answers: {
-          musical_expertise: null,
-          musical_background: null,
-          most_engaged: null,
-          hearing_impairments: null,
-          visual_impairments: null,
-          nationality: null,
-          dob: null,
-          sex: null,
-          music_styles: [],
-          most_enjoyed: null,
-          emotion_indices: [],
-          ratings: {
-            positivity: [],
-            like_dislike: [],
-            familiarity: [],
-            engagement: [],
-            activity: [],
-            chills: [],
-            power: []
-          }
-        },
-        date: null,
-        media: [],
-        timestamps: {
-          start: null,
-          test: null,
-          media: [],
-          end: null
-        },
-        metadata: {
-          session_number: null,
-          location: 'taipei_city',
-          terminal: null
-        },
-        state: {
-          currentSlideIndex: -1,
-          mediaPlayCount: 0
+        function BlankDataObject() {
+            return {
+                answers: {
+                    musical_expertise: null,
+                    musical_background: null,
+                    most_engaged: null,
+                    hearing_impairments: null,
+                    visual_impairments: null,
+                    nationality: null,
+                    dob: null,
+                    sex: null,
+                    music_styles: [],
+                    most_enjoyed: null,
+                    emotion_indices: [],
+                    ratings: {
+                        positivity: [],
+                        like_dislike: [],
+                        familiarity: [],
+                        engagement: [],
+                        activity: [],
+                        chills: [],
+                        power: []
+                    }
+                },
+                date: null,
+                media: [],
+                timestamps: {
+                    start: null,
+                    test: null,
+                    media: [],
+                    end: null
+                },
+                metadata: {
+                    session_number: null,
+                    location: 'taipei_city',
+                    terminal: null
+                },
+                state: {
+                    currentSlideIndex: -1,
+                    mediaPlayCount: 0
+                }
+            };
         }
-      };
+
+        var trialData = {
+            data: null,
+
+            toJson: function() {
+                return angular.toJson(this.data, true);
+            },
+
+            reset: function() {
+                this.data = new BlankDataObject();
+            },
+
+            setValueForPath: function(path, value, options) {
+                if (path && value) {
+                    var schema = this;  // a moving reference to internal objects within this (this TrialData)
+                    var pList = path.split('.');
+                    var len = pList.length;
+                    for (var i = 0; i < len - 1; i++) {
+                        var elem = pList[i];
+                        if (!schema[elem]) schema[elem] = {};
+                        schema = schema[elem];
+                    }
+
+                    if (options && options.hasOwnProperty('array_index') && typeof options.array_index === 'number') {
+                        schema[pList[len - 1]][options.array_index] = value;
+                    } else {
+                        schema[pList[len - 1]] = value;
+                    }
+                }
+            },
+
+            // FIXME: Need to gracefully handle bad array indices
+            setValueForPathForCurrentMedia: function(path, value) {
+
+                var index;
+
+                // If no media have played (we're likely debugging)
+                if (this.data.state.mediaPlayCount <= 0) {
+                    index = 0;
+                } else {
+                    index = this.data.state.mediaPlayCount - 1;
+                }
+
+                this.setValueForPath(path, value, {array_index: index});
+            }
+        };
+
+        trialData.data = new BlankDataObject();
+
+        return trialData;
     }
-
-    var trialData = {
-      data: {
-        answers: {
-          musical_expertise: null,
-          musical_background: null,
-          most_engaged: null,
-          hearing_impairments: null,
-          visual_impairments: null,
-          nationality: null,
-          dob: null,
-          sex: null,
-          music_styles: [],
-          most_enjoyed: null,
-          emotion_indices: [],
-          ratings: {
-            positivity: [],
-            like_dislike: [],
-            familiarity: [],
-            engagement: [],
-            activity: [],
-            chills: [],
-            power: []
-          }
-        },
-        date: null,
-        media: [],
-        timestamps: {
-          start: null,
-          test: null,
-          media: [],
-          end: null
-        },
-        metadata: {
-          session_number: null,
-          location: 'taipei_city',
-          terminal: null
-        },
-        state: {
-          currentSlideIndex: -1,
-          mediaPlayCount: 0
-        }
-      },
-
-      toJson: function () {
-        return angular.toJson(this.data, true);
-      },
-
-      reset: function () {
-        this.data = blankDataObject();
-      },
-
-      setValueForPath: function(path, value) {
-        if (path && value) {
-          var schema = this;  // a moving reference to internal objects within this (this TrialData)
-          var pList = path.split('.');
-          var len = pList.length;
-          for (var i = 0; i < len - 1; i++) {
-            var elem = pList[i];
-            if (!schema[elem]) schema[elem] = {};
-            schema = schema[elem];
-          }
-
-          schema[pList[len - 1]] = value;
-        }
-      },
-
-      setValueForPathForCurrentMedia: function(path, value) {
-        var schema = this;  // a moving reference to internal objects within this (this TrialData)
-        var pList = path.split('.');
-        var len = pList.length;
-        for(var i = 0; i < len-1; i++) {
-          var elem = pList[i];
-          if( !schema[elem] ) schema[elem] = {};
-          schema = schema[elem];
-        }
-
-        // Check that we are assigning to the right index
-        var index;
-
-        // If no media have played (we're likely debugging)
-        if (this.data.state.mediaPlayCount <= 0) {
-          index = 0;
-        } else {
-          index = this.data.state.mediaPlayCount - 1;
-
-          if (index >= schema[pList[len-1]].length) {
-            index = schema[pList[len-1]].length - 1;
-          }
-        }
-
-        schema[pList[len-1]][index] = value;
-      }
-    };
-
-    return trialData;
-  }
 ]);
---
+++ public/modules/core/tests/trial-data.client.service.test.js
@@ -0,0 +1,194 @@
+'use strict';
+
+(function() {
+    describe('TrialData', function() {
+
+        //Initialize global variables
+        var TrialData;
+        var blankDataObject = {
+            answers: {
+                musical_expertise: null,
+                musical_background: null,
+                most_engaged: null,
+                hearing_impairments: null,
+                visual_impairments: null,
+                nationality: null,
+                dob: null,
+                sex: null,
+                music_styles: [],
+                most_enjoyed: null,
+                emotion_indices: [],
+                ratings: {
+                    positivity: [],
+                    like_dislike: [],
+                    familiarity: [],
+                    engagement: [],
+                    activity: [],
+                    chills: [],
+                    power: []
+                }
+            },
+
+            date: null,
+            media: [],
+            timestamps: {
+                start: null,
+                test: null,
+                media: [],
+                end: null
+            },
+            metadata: {
+                session_number: null,
+                location: 'taipei_city',
+                terminal: null
+            },
+            state: {
+                currentSlideIndex: -1,
+                mediaPlayCount: 0
+            }
+        };
+
+        // Load the main application module
+        beforeEach(module(ApplicationConfiguration.applicationModuleName));
+
+        beforeEach(inject(function(_TrialData_) {
+            TrialData = _TrialData_;
+        }));
+
+        describe('initialization', function() {
+            it('should be defined', function() {
+                expect(TrialData).toBeDefined();
+            });
+
+            it('should return an object with a properly formatted data member', function() {
+                expect(TrialData.data).toBeDefined();
+                expect(TrialData.data.metadata).toBeDefined();
+                expect(TrialData.data.state).toBeDefined();
+                expect(TrialData.data.answers).toBeDefined();
+                expect(TrialData.data.answers.musical_expertise).toBeNull();
+                expect(TrialData.data.answers.musical_background).toBeNull();
+                expect(TrialData.data.answers.most_engaged).toBeNull();
+                expect(TrialData.data.answers.hearing_impairments).toBeNull();
+                expect(TrialData.data.answers.visual_impairments).toBeNull();
+                expect(TrialData.data.answers.nationality).toBeNull();
+                expect(TrialData.data.answers.dob).toBeNull();
+                expect(TrialData.data.answers.sex).toBeNull();
+                expect(TrialData.data.answers.music_styles).toBeDefined();
+                expect(TrialData.data.answers.music_styles.length).toBe(0);
+                expect(TrialData.data.answers.most_enjoyed).toBeNull();
+                expect(TrialData.data.answers.emotion_indices).toBeDefined();
+                expect(TrialData.data.answers.emotion_indices.length).toBe(0);
+                expect(TrialData.data.answers.ratings).toBeDefined();
+                expect(TrialData.data.answers.ratings.positivity).toBeDefined();
+                expect(TrialData.data.answers.ratings.positivity.length).toBe(0);
+                expect(TrialData.data.answers.ratings.like_dislike).toBeDefined();
+                expect(TrialData.data.answers.ratings.like_dislike.length).toBe(0);
+                expect(TrialData.data.answers.ratings.familiarity).toBeDefined();
+                expect(TrialData.data.answers.ratings.familiarity.length).toBe(0);
+                expect(TrialData.data.answers.ratings.engagement).toBeDefined();
+                expect(TrialData.data.answers.ratings.engagement.length).toBe(0);
+                expect(TrialData.data.answers.ratings.activity).toBeDefined();
+                expect(TrialData.data.answers.ratings.activity.length).toBe(0);
+                expect(TrialData.data.answers.ratings.chills).toBeDefined();
+                expect(TrialData.data.answers.ratings.chills.length).toBe(0);
+                expect(TrialData.data.answers.ratings.power).toBeDefined();
+                expect(TrialData.data.answers.ratings.power.length).toBe(0);
+                expect(TrialData.data.date).toBeNull();
+                expect(TrialData.data.media).toBeDefined();
+                expect(TrialData.data.media.length).toBe(0);
+                expect(TrialData.data.timestamps).toBeDefined();
+                expect(TrialData.data.timestamps.start).toBeNull();
+                expect(TrialData.data.timestamps.test).toBeNull();
+                expect(TrialData.data.timestamps.end).toBeNull();
+                expect(TrialData.data.timestamps.media).toBeDefined();
+                expect(TrialData.data.timestamps.media.length).toBe(0);
+                expect(TrialData.data.metadata.session_number).toBeNull();
+                expect(TrialData.data.metadata.location).toBe('taipei_city');
+                expect(TrialData.data.metadata.terminal).toBeNull();
+                expect(TrialData.data.state.currentSlideIndex).toBe(-1);
+                expect(TrialData.data.state.mediaPlayCount).toBe(0);
+            });
+        });
+
+        describe('#toJson', function() {
+            it('should be defined', function() {
+                expect(TrialData.toJson).toBeDefined();
+                expect(typeof TrialData.toJson).toBe('function');
+            });
+
+            it('should return a JSON representation of the data property', function() {
+                var mockData = {foo: 'bar'};
+                TrialData.data = mockData;
+                expect(TrialData.toJson()).toEqual(angular.toJson(mockData, true));
+            });
+        });
+
+        describe('#reset', function() {
+            it('should be defined', function() {
+                expect(TrialData.reset).toBeDefined();
+                expect(typeof TrialData.reset).toBe('function');
+            });
+
+            it('should return a blank data object', function() {
+                var mockData = {foo: 'bar'};
+                TrialData.data = mockData;
+                TrialData.reset();
+                expect(TrialData.data).toEqual(blankDataObject);
+            });
+        });
+
+        describe('data persistence', function() {
+            it('should persist changes to the data property', function() {
+                TrialData.data.answers.musical_expertise = 'damn good';
+                expect(TrialData.data.answers.musical_expertise).toBe('damn good');
+            });
+
+            describe('#setValueForPath', function() {
+                it('should set a simple path\'s value', function() {
+                    TrialData.data.answers.hearing_impairments = false;
+                    TrialData.setValueForPath('data.answers.hearing_impairments', true);
+                    expect(TrialData.data.answers.hearing_impairments).toBe(true);
+                });
+
+                it('shouldn\'t do anything if a value is not supplied', function() {
+                    TrialData.data.answers.hearing_impairments = false;
+                    TrialData.setValueForPath('data.answers.hearing_impairments');
+                    expect(TrialData.data.answers.hearing_impairments).toBe(false);
+                });
+
+                it('should correctly set a value for deep path that doesn\'t yet exist', function() {
+                    TrialData.setValueForPath('foo.bar.baz', 42);
+                    expect(TrialData.foo.bar.baz).toBe(42);
+                });
+
+                it('should set a simple path\'s value and respect the array_index option', function() {
+                    TrialData.data.answers.emotion_indices = ['a','b','c'];
+                    TrialData.setValueForPath('data.answers.emotion_indices', 'z', {array_index:2});
+                    expect(TrialData.data.answers.emotion_indices[2]).toBe('z');
+                });
+            });
+
+            describe('#setValueForPathForCurrentMedia', function() {
+                it('should set a simple path\'s value when no media have played', function() {
+                    TrialData.data.answers.emotion_indices = ['a','b','c'];
+                    TrialData.data.state.mediaPlayCount = 0;
+                    TrialData.setValueForPathForCurrentMedia('data.answers.emotion_indices', 'z');
+                    expect(TrialData.data.answers.emotion_indices[0]).toBe('z');
+                });
+
+                it('should set a simple path\'s value for the first index with a blank data object', function() {
+                    TrialData.data.answers.emotion_indices = ['a','b','c'];
+                    TrialData.setValueForPathForCurrentMedia('data.answers.emotion_indices', 'z');
+                    expect(TrialData.data.answers.emotion_indices[0]).toBe('z');
+                });
+
+                it('should set a simple path\'s value when some media have played', function() {
+                    TrialData.data.answers.emotion_indices = ['a','b','c'];
+                    TrialData.data.state.mediaPlayCount = 2;
+                    TrialData.setValueForPathForCurrentMedia('data.answers.emotion_indices', 'z');
+                    expect(TrialData.data.answers.emotion_indices[1]).toBe('z');
+                });
+            });
+        });
+    });
+})();
