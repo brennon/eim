fcf9ed9400b3c657e64361e5c305ae05baf47a40
Brennon Bortz <brennon@brennonbortz.com>
2014-10-23 12:44:14 -0400

    Reformatted HomeController

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,24 +1,24 @@
 'use strict';
 
 angular.module('core').controller('HomeController', ['ExperimentManager', '$scope',
-	function(ExperimentManager, $scope) {
+    function(ExperimentManager, $scope) {
 
-    // Bind $scope.advanceSlide to ExperimentManager functionality
-    $scope.advanceSlide = ExperimentManager.advanceSlide;
+        // Bind $scope.advanceSlide to ExperimentManager functionality
+        $scope.advanceSlide = ExperimentManager.advanceSlide;
 
-    // Reset ExperimentManager for new trial
-    ExperimentManager.masterReset().then(
-      function experimentResetSuccessHandler () {
-        $scope.readyToAdvance = true;
-      },
-      function experimentResetErrorHandler () {
-        $scope.addGenericErrorAlert();
-        throw new Error('An experiment schema could not be fetched from the server');
-      }
-    );
+        // Reset ExperimentManager for new trial
+        ExperimentManager.masterReset().then(
+            function experimentResetSuccessHandler() {
+                $scope.readyToAdvance = true;
+            },
+            function experimentResetErrorHandler() {
+                $scope.addGenericErrorAlert();
+                throw new Error('An experiment schema could not be fetched from the server');
+            }
+        );
 
-    // Ready to move to next slide?
-    $scope.readyToAdvance = false;
+        // Ready to move to next slide?
+        $scope.readyToAdvance = false;
 
-	}
+    }
 ]);
