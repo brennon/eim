45f69697d2f34713a5dfda6cf2d0203bfe56af5b
Brennon Bortz <brennon@brennonbortz.com>
2014-10-24 15:19:45 -0400

    MediaPlaybackController tests

--- public/modules/core/controllers/media-playback.client.controller.js
+++ public/modules/core/controllers/media-playback.client.controller.js
@@ -1,121 +1,91 @@
 'use strict';
 
-// TODO: Address OSC error when message sent without session ID
+// FIXME: Address OSC error when message sent without session ID
 // TODO: Remove blackout when returning home
 // TODO: Use different button colors for Playback and Continue
 // TODO: Find a better way to listen for socket messages and clean up listeners
 
-angular.module('core').controller('MediaPlaybackController', ['$scope', 'TrialData', '$state', 'ExperimentManager',
-  function ($scope, TrialData, $state, ExperimentManager) {
+angular.module('core').controller('MediaPlaybackController', ['$scope', 'TrialData', 'SocketIOService', '$timeout', /*'$state', 'ExperimentManager',*/
+    function($scope, TrialData, SocketIOService, $timeout/*, $state, ExperimentManager*/) {
 
-    // Bind $scope.advanceSlide to ExperimentManager functionality
-    $scope.advanceSlide = ExperimentManager.advanceSlide;
+        // State to control button behavior
+        $scope.currentButtonLabel = 'Begin Playback';
+        $scope.mediaHasPlayed = false;
 
-    /* global io */
-    var socket = io();
+        // Send media playback message to Max
+        $scope.startMediaPlayback = function() {
 
-    // State to control button behavior
-    $scope.currentButtonLabel = 'Begin Playback';
-    $scope.mediaHasPlayed = false;
+            var mediaIndex = TrialData.data.state.mediaPlayCount;
+            var mediaLabel = TrialData.data.media[mediaIndex];
 
-    // Send media playback message to Max
-    $scope.startMediaPlayback = function () {
-
-      var mediaIndex = TrialData.data.state.mediaPlayCount;
-      var mediaLabel = TrialData.data.media[mediaIndex];
-
-      socket.emit('sendOSCMessage', {
-        oscType: 'message',
-        address: '/eim/control/mediaID',
-        args: [
-          {
-            type: 'string',
-            value: '' + mediaLabel
-          },
-          {
-            type: 'string',
-            value: '' + TrialData.data.metadata.session_number
-          }
-        ]
-      });
-    };
-
-    $scope.trialDataJson = function () {
-      return TrialData.toJson();
-    };
-
-    /*
-    // Determine next state and send the user there
-    $scope.gotoNextState = function () {
-      if (!$scope.mediaHasPlayed) {
-        $scope.startMediaPlayback();
-      } else if (TrialData.data.state.mediaPlayCount >= TrialData.data.media.length) {
-        $state.go('home');
-      } else if (TrialData.data.state.mediaPlayCount !== TrialData.data.media.length) {
-        $state.go('media-playback', {}, {reload: true});
-      }
-    };
-    */
-
-    // Setup listener for incoming OSC messages
-    function oscMessageReceivedListener(data) {
-
-      console.log('got message');
-
-      // If it was a media playback message
-      if (data.address === '/eim/status/playback') {
-
-        console.log('got playback message');
-
-        // If it was a start message
-        if (parseInt(data.args[0].value) === 1) {
-          console.log('got start message');
-
-          // Fade out screen
-          $scope.hideBody();
-
-          // Otherwise, if it was a stop message
-        } else if (parseInt(data.args[0].value) === 0) {
-
-          console.log('got stop message');
-
-          // Fade in screen
-          $scope.showBody();
-
-          if (!$scope.mediaHasPlayed) {
-
-            // Increment media play count
-            TrialData.data.state.mediaPlayCount++;
-
-            // Update state
-            $scope.$apply(function() {
-              $scope.currentButtonLabel = 'Continue';
-              $scope.mediaHasPlayed = true;
+            SocketIOService.emit('sendOSCMessage', {
+                oscType: 'message',
+                address: '/eim/control/mediaID',
+                args: [
+                    {
+                        type: 'string',
+                        value: '' + mediaLabel
+                    },
+                    {
+                        type: 'string',
+                        value: '' + TrialData.data.metadata.session_number
+                    }
+                ]
             });
-          }
-        }
-      }
+        };
 
-      console.log('currentButtonLabel: ' + $scope.currentButtonLabel);
-      console.log('mediaHasPlayed: ' + $scope.mediaHasPlayed);
+        // Setup listener for incoming OSC messages
+        this.oscMessageReceivedListener = function(data) {
+
+            // If it was a media playback message
+            if (data.address === '/eim/status/playback') {
+
+                // If it was a start message
+                if (parseInt(data.args[0].value) === 1) {
+
+                    // Fade out screen
+                    $scope.hideBody();
+
+                    // Otherwise, if it was a stop message
+                } else if (parseInt(data.args[0].value) === 0) {
+
+                    // Fade in screen
+                    $scope.showBody();
+
+                    if (!$scope.mediaHasPlayed) {
+
+                        // Increment media play count
+                        TrialData.data.state.mediaPlayCount++;
+
+                        // Update state
+                        $timeout(function() {
+                            $scope.$apply(function() {
+                                $scope.currentButtonLabel = 'Continue';
+                                $scope.mediaHasPlayed = true;
+                            });
+                        });
+                    }
+                }
+            }
+        };
+
+        // Attach OSC message received listener
+        SocketIOService.on('oscMessageReceived', this.oscMessageReceivedListener);
+
+        // Send a message to stop media playback when this controller is destroyed
+        // Also, remove OSC message event listeners
+        var thisController = this;
+        $scope.$on('$destroy', function stopMediaPlayback() {
+            SocketIOService.emit('sendOSCMessage', {
+                oscType: 'message',
+                address: '/eim/control/stopMedia',
+                args: {
+                    type: 'string',
+                    value: '' + TrialData.data.metadata.session_number
+                }
+            });
+
+            SocketIOService.removeListener('oscMessageReceived', thisController.oscMessageReceivedListener);
+        });
     }
-
-    // Attach OSC message received listener
-    socket.on('oscMessageReceived', oscMessageReceivedListener);
-
-    // Send a message to stop media playback when this controller is destroyed
-    // Also, remove OSC message event listeners
-    $scope.$on('$destroy', function stopMediaPlayback() {
-      socket.emit('sendOSCMessage', {
-        oscType: 'message',
-        address: '/eim/control/stopMedia',
-        args: {
-          type: 'string',
-          value: '' + TrialData.data.metadata.session_number
-        }
-      });
-
-      socket.removeListener('oscMessageReceived', oscMessageReceivedListener);
-    });
-  }
 ]);
---
+++ public/modules/core/tests/media-playback.client.controller.test.js
@@ -0,0 +1,254 @@
+'use strict';
+
+(function() {
+    describe('MediaPlaybackController', function() {
+
+        //Initialize global variables
+        var mockScope, $controller, SocketIOService, $timeout;
+
+        // Load the main application module
+        beforeEach(module(ApplicationConfiguration.applicationModuleName));
+
+        beforeEach(inject(function(_$controller_, $rootScope, _SocketIOService_, _$timeout_) {
+            $controller = _$controller_;
+            mockScope = $rootScope.$new();
+            SocketIOService = _SocketIOService_;
+            $timeout = _$timeout_;
+        }));
+
+        describe('initialization', function() {
+            it('should be defined', function() {
+                var createController = function() {
+                    $controller('MediaPlaybackController',
+                        {$scope: mockScope}
+                    );
+                };
+
+                expect(createController).not.toThrow();
+            });
+        });
+
+        describe('view/state manipulation', function() {
+            it('should set the button label to \'Begin Playback\'', function() {
+                $controller('MediaPlaybackController',
+                    {$scope: mockScope}
+                );
+
+                expect(mockScope.currentButtonLabel).toBe('Begin Playback');
+            });
+
+            it('should set mediaHasPlayed to false', function() {
+                $controller('MediaPlaybackController',
+                    {$scope: mockScope}
+                );
+
+                expect(mockScope.mediaHasPlayed).toBe(false);
+            });
+        });
+
+        describe('OSC messages', function() {
+            it('$scope.startMediaPlayback should send the correct start message to the socket', function() {
+
+                var mockTrialData = {
+                    data: {
+                        media: ['a', 'b', 'c', 'd'],
+                        state: {
+                            mediaPlayCount: 3
+                        },
+                        metadata: {
+                            session_number: 42
+                        }
+                    }
+                };
+
+                $controller('MediaPlaybackController',
+                    {$scope: mockScope, TrialData: mockTrialData}
+                );
+
+                SocketIOService.emits = {};
+                mockScope.startMediaPlayback();
+                var emittedMessage = SocketIOService.emits.sendOSCMessage[0][0];
+                expect(emittedMessage).toEqual(
+                    {
+                        oscType: 'message',
+                        address: '/eim/control/mediaID',
+                        args: [
+                            {
+                                type: 'string',
+                                value: mockTrialData.data.media[mockTrialData.data.state.mediaPlayCount]
+                            },
+                            {
+                                type: 'string',
+                                value: '' + mockTrialData.data.metadata.session_number
+                            }
+                        ]
+                    }
+                );
+            });
+
+            describe('message received listener', function() {
+                it('should be bound to the oscMessageReceived event', function() {
+                    spyOn(SocketIOService, 'on');
+                    var controller = $controller('MediaPlaybackController',
+                        {$scope: mockScope}
+                    );
+                    expect(SocketIOService.on.calls.count()).toBe(1);
+                    expect(SocketIOService.on.calls.argsFor(0)[0]).toBe('oscMessageReceived');
+                    expect(SocketIOService.on.calls.argsFor(0)[1]).toBe(controller.oscMessageReceivedListener);
+                });
+
+                it('should call $scope.hideBody on a playback start message', function() {
+                    mockScope.hideBody = function() {
+                    };
+                    $controller('MediaPlaybackController',
+                        {$scope: mockScope}
+                    );
+                    spyOn(mockScope, 'hideBody');
+                    SocketIOService.receive('oscMessageReceived',
+                        {
+                            address: '/eim/status/playback',
+                            args: [
+                                {
+                                    value: 1
+                                }
+                            ]
+                        }
+                    );
+                    expect(mockScope.hideBody.calls.count()).toBe(1);
+                });
+
+                it('should call $scope.showBody on a playback stop message', function() {
+                    mockScope.showBody = function() {
+                    };
+                    $controller('MediaPlaybackController',
+                        {$scope: mockScope}
+                    );
+                    spyOn(mockScope, 'showBody');
+                    SocketIOService.receive('oscMessageReceived',
+                        {
+                            address: '/eim/status/playback',
+                            args: [
+                                {
+                                    value: 0
+                                }
+                            ]
+                        }
+                    );
+                    expect(mockScope.showBody.calls.count()).toBe(1);
+                });
+
+                it('should increment TrialData play count on a playback stop message if media has not yet played', function() {
+                    mockScope.showBody = function() {
+                    };
+                    var mockTrialData = {
+                        data: {
+                            state: {
+                                mediaPlayCount: 0
+                            }
+                        }
+                    };
+                    $controller('MediaPlaybackController',
+                        {$scope: mockScope, TrialData: mockTrialData}
+                    );
+
+                    mockScope.mediaHasPlayed = false;
+                    SocketIOService.receive('oscMessageReceived',
+                        {
+                            address: '/eim/status/playback',
+                            args: [
+                                {
+                                    value: 0
+                                }
+                            ]
+                        }
+                    );
+                    expect(mockTrialData.data.state.mediaPlayCount).toBe(1);
+                });
+
+                it('should update the button label on a playback stop message if media has not yet played', function() {
+                    mockScope.showBody = function() {
+                    };
+                    $controller('MediaPlaybackController',
+                        {$scope: mockScope}
+                    );
+
+                    mockScope.mediaHasPlayed = false;
+                    mockScope.currentButtonLabel = '';
+                    SocketIOService.receive('oscMessageReceived',
+                        {
+                            address: '/eim/status/playback',
+                            args: [
+                                {
+                                    value: 0
+                                }
+                            ]
+                        }
+                    );
+
+                    $timeout.flush();
+
+                    expect(mockScope.currentButtonLabel).toBe('Continue');
+                });
+
+                it('should set mediaHasPlayed to true on a playback stop message if media has not yet played', function() {
+                    mockScope.showBody = function() {
+                    };
+                    $controller('MediaPlaybackController',
+                        {$scope: mockScope}
+                    );
+
+                    mockScope.mediaHasPlayed = false;
+                    SocketIOService.receive('oscMessageReceived',
+                        {
+                            address: '/eim/status/playback',
+                            args: [
+                                {
+                                    value: 0
+                                }
+                            ]
+                        }
+                    );
+
+                    $timeout.flush();
+
+                    expect(mockScope.mediaHasPlayed).toBe(true);
+                });
+
+                it('should be removed when the scope is destroyed', function() {
+                    $controller('MediaPlaybackController',
+                        {$scope: mockScope}
+                    );
+
+                    spyOn(SocketIOService, 'removeListener');
+                    mockScope.$destroy();
+                    expect(SocketIOService.removeListener.calls.count()).toBe(1);
+                });
+            });
+
+            it('should send a stopMedia message when the scope is destroyed', function() {
+                var mockTrialData = {
+                    data: {
+                        metadata: {
+                            session_number: 42
+                        }
+                    }
+                }
+                $controller('MediaPlaybackController',
+                    {$scope: mockScope, TrialData: mockTrialData}
+                );
+
+                SocketIOService.emits = {};
+                mockScope.$destroy();
+                var emittedMessage = SocketIOService.emits.sendOSCMessage[0][0]
+                expect(emittedMessage).toEqual({
+                    oscType: 'message',
+                    address: '/eim/control/stopMedia',
+                    args: {
+                        type: 'string',
+                        value: '' + mockTrialData.data.metadata.session_number
+                    }
+                });
+            });
+        });
+    });
+})();
