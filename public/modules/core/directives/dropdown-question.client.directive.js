0ab2110c0e53d147345c965c7576a6ae5a6fff51
Brennon Bortz <brennon@brennonbortz.com>
2014-11-03 22:10:02 -0500

    Add dropdown-question directive

---
+++ public/modules/core/directives/dropdown-question.client.directive.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// FIXME: Binding radio options to element's data sucks
+
+angular.module('core').directive('dropdownQuestion', ['$compile', 'TrialData', function($compile, TrialData) {
+
+    return {
+        restrict: 'E',
+        scope: {},
+
+        link: function(scope, element, attrs) {
+
+            scope.sendToTrialData = function(path, value) {
+                if (!attrs.associatedToMedia) {
+                    TrialData.setValueForPath(path, value);
+                } else {
+                    TrialData.setValueForPathForCurrentMedia(path, value);
+                }
+            };
+
+            scope[attrs.questionId + 'Select'] = 'bar';
+
+            scope.$watch(attrs.questionId + 'Select', function(newValue) {
+                scope.sendToTrialData(attrs.controllerDataPath, newValue);
+            });
+
+            if (element.data('dropdownOptions')) {
+                scope.dropdownOptions = element.data('dropdownOptions');
+            }
+
+            var selectElement = angular.element('<div class="row"><div class="col-md-12 form-group"><label for="' + attrs.questionId + '" class="control-label">' + attrs.questionLabel + '</label><select id="'+attrs.questionId+'" name="'+attrs.questionId+'" class="form-control" ng-options="option for option in dropdownOptions" ng-model="'+attrs.questionId+'Select"></select></div></div>');
+
+            element.append(selectElement);
+            $compile(element.contents())(scope);
+        }
+    };
+}]);
