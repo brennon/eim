306c311271ea9b78b4b2f0dd920f882043470262
Brennon Bortz <brennon@brennonbortz.com>
2014-11-12 15:44:37 -0500

    Correctly use fixed media from schema

--- app/models/experiment-schema.server.model.js
+++ app/models/experiment-schema.server.model.js
@@ -4,29 +4,30 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema,
-  MediaSchema = require('./media.server.model').schema,
-  _ = require('lodash');
+    Schema = mongoose.Schema,
+    MediaSchema = require('./media.server.model').schema,
+    _ = require('lodash'),
+    BluebirdPromise = require('bluebird');
 
 /**
  * ExperimentSchema Schema
  */
 var ExperimentSchemaSchema = new Schema({
-  structure: {},
-  sensors: [String],
-	trialCount: {
-    type: Number,
-    required: true
-  },
-  mediaPool: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Media'
-  }]
+    structure: {},
+    sensors: [String],
+    trialCount: {
+        type: Number,
+        required: true
+    },
+    mediaPool: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Media'
+    }]
 });
 
 // Validator that requires the value is an array and contains at least one entry
 function requiredArrayValidator(value) {
-  return Object.prototype.toString.call(value) === '[object Array]' && value.length > 0;
+    return Object.prototype.toString.call(value) === '[object Array]' && value.length > 0;
 }
 
 // Use the above validator for `mediaPool`
@@ -34,24 +35,58 @@ ExperimentSchemaSchema.path('mediaPool').validate(requiredArrayValidator);
 
 ExperimentSchemaSchema.methods.buildExperiment = function(callback) {
 
-  // Populate mediaPool
-  this.populate('mediaPool');
+    // Populate mediaPool
+    this.populate('mediaPool');
 
-  var selectedMedia = _.sample(this.mediaPool, this.trialCount);
+    var selectedMedia = _.sample(this.mediaPool, this.trialCount);
 
-  var schemaSubset = {
-    _id: this._id,
-    structure: this.structure,
-    sensors: this.sensors,
-    media: selectedMedia,
-  };
+    var schemaSubset = {
+        _id: this._id,
+        structure: this.structure,
+        sensors: this.sensors,
+        media: selectedMedia,
+    };
 
-  if (typeof callback === 'function') {
-    return callback(null, schemaSubset);
-  } else {
-    return schemaSubset;
-  }
+    // Iterate over structure to look for fixed media-playback slides
+    var indexInMedia = 0;
+    var promises = [];
+    for (var i = 0; i < schemaSubset.structure.length; i++) {
+        var slide = schemaSubset.structure[i];
+
+        if (slide.name === 'media-playback' && slide.mediaType === 'fixed') {
+            promises.push(updateSchemaForFixedMedia(schemaSubset, indexInMedia, slide.media));
+            indexInMedia++;
+        } else if (slide.name === 'media-playback' && slide.mediaType === 'random') {
+            indexInMedia++;
+        }
+    }
+
+    BluebirdPromise.all(promises).then(function() {
+        if (typeof callback === 'function') {
+            return callback(null, schemaSubset);
+        } else {
+            return schemaSubset;
+        }
+    });
 };
+
+function updateSchemaForFixedMedia(schema, index, mediaId) {
+    return new BluebirdPromise(function(resolve, reject) {
+        // Get the media entry for the ObjectID in slide.media
+        var MediaModel = mongoose.model('Media');
+        MediaModel.findOne({_id: mediaId}, {_id:1, artist:1, title: 1, label:1}, function(err, media) {
+            if (err) {
+                reject();
+            }
+
+            if (media) {
+                console.log('resolving');
+                schema.media[index] = media;
+                resolve();
+            }
+        });
+    });
+}
 
 // Register schema for `ExperimentSchema` model
 mongoose.model('ExperimentSchema', ExperimentSchemaSchema);
