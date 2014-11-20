e269ffb69f46dec154dfd1855d61dcfe20160c03
Brennon Bortz <brennon@brennonbortz.com>
2014-11-19 20:01:35 -0500

    Show timing information in E2E tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,7 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['public/e2e/*.js']
+    specs: ['public/e2e/*.js'],
+    jasmineNodeOpts: {
+        showTiming: true
+    }
 };
