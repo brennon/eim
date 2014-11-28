43359d9898e013319ee1041931af35d907fbf8f0
Brennon Bortz <brennon@brennonbortz.com>
2014-11-28 12:46:11 -0500

    Save English option to TrialData on translated dropdown-question directives
     Fixes #51

--- public/modules/core/directives/dropdown-question.client.directive.js
+++ public/modules/core/directives/dropdown-question.client.directive.js
@@ -26,7 +26,7 @@ angular.module('core').directive('dropdownQuestion', ['$compile', 'TrialData', f
                 scope.dropdownOptions = element.data('dropdownOptions');
             }
 
-            var selectElement = angular.element('<div class="row well"><div class="col-md-12 form-group"><label for="' + attrs.questionId + '" class="control-label" translate>' + attrs.questionLabel + '</label><select id="'+attrs.questionId+'" name="'+attrs.questionId+'" class="form-control" ng-model="'+attrs.questionId+'Select" required><option ng-repeat="option in dropdownOptions">{{option | translate}}</option> </select></div></div>');
+            var selectElement = angular.element('<div class="row well"><div class="col-md-12 form-group"><label for="' + attrs.questionId + '" class="control-label" translate>' + attrs.questionLabel + '</label><select id="'+attrs.questionId+'" name="'+attrs.questionId+'" class="form-control" ng-model="'+attrs.questionId+'Select" required><option ng-repeat="option in dropdownOptions" value="{{option}}">{{option | translate}}</option> </select></div></div>');
 
             element.append(selectElement);
             $compile(element.contents())(scope);
