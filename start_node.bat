513453d2cd1fd23cbf1a611a61cd79ae47f69f9e
Brennon Bortz <brennon@brennonbortz.com>
2014-12-07 13:37:37 +0800

    Update startup script

--- start_node.bat
+++ start_node.bat
@@ -1,4 +1,11 @@
+REM Start Max patch
+start "" "C:\Program Files\Cycling '74\Max Runtime 6.1\maxRT.exe" C:\eim\EiMpatch\EmotionInMotion.maxproj
+
+REM Set Node.js environment to production
 set NODE_ENV=production
+
+REM Switch to EiM directory
 cd c:\eim
-# forever -c node server.js
+
+REM Start Node.js server
 C:\Users\EmotionInMotionV2\AppData\Roaming\npm\grunt default
