3155589948db2e0181d4f78c46e0b4b8dfe48eb5
Brennon Bortz <brennon@brennonbortz.com>
2014-12-08 12:14:32 +0800

    Hide TrialData by default

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -1,7 +1,10 @@
 'use strict';
 
 module.exports = function(app) {
-	// Root routing
-	var core = require('../../app/controllers/core');
-  app.route('/').get(core.index);
+    // Root routing
+    var core = require('../../app/controllers/core');
+    app.route('/').get(core.index);
+    app.route('/api/nodeenv').get(function(req, res, next) {
+        return res.json({ env: app.get('env') });
+    });
 };
--- public/modules/core/directives/display-trial-data.client.directive.js
+++ public/modules/core/directives/display-trial-data.client.directive.js
@@ -2,8 +2,9 @@
 
 // Display Trial Data for debugging
 angular.module('core').directive('displayTrialData', function() {
-  return {
-    restrict: 'AE',
-    template: '<div class="row"><div class="col-md-12"><h3>Trial Data</h3><pre>{{trialDataJson()}}</pre></div></div>'
-  };
+
+    return {
+        restrict: 'AE',
+        template: '<div class="row trial-data-row"><div class="col-md-12"><h3>Trial Data</h3><pre>{{trialDataJson()}}</pre></div></div>'
+    };
 });
