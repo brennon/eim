201e8bd6a52beb5d6ac1977aea782ece2e1ca823
Brennon Bortz <brennon@brennonbortz.com>
2015-09-15 09:13:31 -0400

    Remove superfluous logging statements

--- public/modules/core/controllers/last-screen.client.controller.js
+++ public/modules/core/controllers/last-screen.client.controller.js
@@ -3,9 +3,7 @@
 angular.module('core').controller('LastScreenController', ['$scope', 'TrialData', '$http',
     function($scope, TrialData, $http) {
         $http.post('/api/trials', TrialData.data).success(function() {
-            console.log('success');
         }).error(function() {
-            console.log('error');
         });
     }
 ]);
