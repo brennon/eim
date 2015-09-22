6ede18bb98dd742d4addb5fb08590e2455c45d45
Brennon Bortz <brennon@brennonbortz.com>
2015-09-22 15:55:55 -0400

    Reformat core.server.controller.test.js

--- app/tests/controllers/core.server.controller.test.js
+++ app/tests/controllers/core.server.controller.test.js
@@ -1,16 +1,16 @@
 /**
- * Created by brennon on 8/6/14.
+ * Created by Brennon Bortz on 8/6/14.
  */
 
 'use strict';
 
-var request = require('supertest');
+// Config module
 var config = require('../../../config/config');
-var should = require('chai').should();
 
-/* jshint ignore:start */
+// Testing tools
+var request = require('supertest');
+var should = require('chai').should();
 var sinon = require('sinon');
-/* jshint ignore:end */
 
 describe('Core Controller Tests', function() {
 
@@ -21,7 +21,9 @@ describe('Core Controller Tests', function() {
 
     it('should be in use', function(done) {
       agent.get('/').expect('set-cookie', /connect\.sid.*/).end(function(err, res) {
-        if (err) return done(err);
+        if (err) {
+            return done(err);
+        }
         cookieA = res.header['set-cookie'];
         done();
       });
@@ -29,7 +31,9 @@ describe('Core Controller Tests', function() {
 
     it('should be reset on any visit to the index', function(done) {
       agent.get('/').expect('set-cookie', /connect\.sid.*/).end(function(err, res) {
-        if (err) return done(err);
+        if (err) {
+            return done(err);
+        }
         cookieB = res.header['set-cookie'];
         cookieA.should.not.equal(cookieB);
         done();
