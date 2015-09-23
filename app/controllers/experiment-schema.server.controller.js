0ac83e23cda24d7a330f5f109513328b95430ef3
Brennon Bortz <brennon@brennonbortz.com>
2015-09-23 11:32:20 -0400

    Fix tests for experiment-schema.server.controller.js

--- app/controllers/experiment-schema.server.controller.js
+++ app/controllers/experiment-schema.server.controller.js
@@ -4,7 +4,6 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    _ = require('lodash'),
     ExperimentSchema = mongoose.model('ExperimentSchema');
 
 /**
@@ -12,46 +11,51 @@ var mongoose = require('mongoose'),
  */
 exports.list = function(req, res) {
 
-  ExperimentSchema.find({}, function(err, schemae) {
-    if (err) {
-      res.json(500, { error: err });
-    } else {
-      res.json(200, schemae);
-    }
-  });
-};
+    ExperimentSchema.find({}, function(err, schemae) {
 
+        // TODO: Test both branches as part of route testing
+        if (err) {
+            res.json(500, {error: err});
+        } else {
+            res.json(200, schemae);
+        }
+    });
+};
 
 /**
  * Build a random experiment from a random ExperimentSchema
  */
+// TODO: The actual pulling of a random schema should move into the model
 exports.random = function(req, res) {
 
-  function errorHandler(message) {
-    res.json(500, { error: message });
-  }
-
-  ExperimentSchema.count({}, function(err, count) {
-
-    if (err) {
-      return errorHandler(err);
-    } else if (count < 1) {
-      return errorHandler('no experiment schema found in database');
-    } else {
-
-      // Get a random number from [0, totalSchemae)
-      var rand = Math.floor(Math.random() * count);
-
-      // Get a random schema
-      ExperimentSchema.find({}).skip(rand).limit(1).populate('mediaPool', 'artist title label').exec(function (err, schema) {
-
-        // Using the controller, services an experiment from this schema
-        schema[0].buildExperiment(function(err, builtExperiment) {
-
-          // Send the response
-          res.json(200, builtExperiment);
-        });
-      });
+    function errorHandler(message) {
+        res.json(500, {error: message});
     }
-  });
+
+    ExperimentSchema.count({}, function(err, count) {
+
+        // TODO: Test as part of route testing
+        if (err) {
+            return errorHandler(err);
+        } else if (count < 1) {
+            return errorHandler('no experiment schema found in database');
+        } else {
+
+            // Get a random number from [0, number of schema)
+            var rand = Math.floor(Math.random() * count);
+
+            // Get a random schema
+            ExperimentSchema.find({}).skip(rand).limit(1).populate('mediaPool', 'artist title label').exec(function(err, schema) {
+
+                // TODO: Can't really test this as part of the controller
+                // Using the controller, build an experiment from this schema
+                schema[0].buildExperiment(function(err, builtExperiment) {
+
+                    // TODO: Test as part of route testing
+                    // Send the response
+                    res.json(200, builtExperiment);
+                });
+            });
+        }
+    });
 };
--- app/tests/controllers/experiment-schema.server.controller.test.js
+++ app/tests/controllers/experiment-schema.server.controller.test.js
@@ -1,146 +1,107 @@
-//'use strict';
-//
-//var should = require('should');
-//var httpMocks = require('node-mocks-http');
-//var _ = require('lodash');
-//var mongoose = require('mongoose');
-//var util = require('util');
-//var controller, ExperimentSchema, Media;
-//var firstMedia, secondMedia, thirdMedia, fullExperimentSchema;
-//
-//describe('ExperimentSchema Controller Tests', function () {
-//
-//  //// Ensure that we are connected to the database
-//  //// before running any tests
-//  //before(function (done) {
-//  //
-//  //  var connectedCallback = function () {
-//  //    controller = require('../../controllers/experiment-schema');
-//  //    ExperimentSchema = mongoose.model('ExperimentSchema');
-//  //    Media = mongoose.model('Media');
-//  //    mongoose.connection.removeListener('connected', connectedCallback);
-//  //    done();
-//  //  };
-//  //
-//  //  if (mongoose.connection.readyState !== 1) {
-//  //
-//  //    mongoose.connection.on('connected', connectedCallback);
-//  //
-//  //  } else {
-//  //    connectedCallback();
-//  //  }
-//  //});
-//  //
-//  //before(function (done) {
-//  //
-//  //  firstMedia = new Media({
-//  //    type: 'audio',
-//  //    artist: 'The Verve',
-//  //    title: 'Bittersweet Symphony',
-//  //    label: 'R005'
-//  //  });
-//  //
-//  //  secondMedia = new Media({
-//  //    type: 'audio',
-//  //    artist: 'Pearl Jam',
-//  //    title: 'Nothingman',
-//  //    label: 'S003'
-//  //  });
-//  //
-//  //  thirdMedia = new Media({
-//  //    type: 'audio',
-//  //    artist: 'Neil Young',
-//  //    title: 'My My, Hey Hey',
-//  //    label: 'R010'
-//  //  });
-//  //
-//  //  firstMedia.save(function (err) {
-//  //    if (err) {
-//  //      done(err);
-//  //    } else {
-//  //      secondMedia.save(function (err) {
-//  //        if (err) {
-//  //          done(err);
-//  //        } else {
-//  //          thirdMedia.save(function (err) {
-//  //            if (err) {
-//  //              done(err);
-//  //            } else {
-//  //              fullExperimentSchema = new ExperimentSchema({
-//  //                trialCount: 3,
-//  //                mediaPool: [firstMedia._id, secondMedia._id, thirdMedia._id]
-//  //              });
-//  //              fullExperimentSchema.save(function (err) {
-//  //                if (err) {
-//  //                  done(err);
-//  //                } else {
-//  //                  done();
-//  //                }
-//  //              });
-//  //            }
-//  //          });
-//  //        }
-//  //      });
-//  //    }
-//  //  });
-//  //});
-//  //
-//  //describe('#list', function () {
-//  //
-//  //  it('should respond with JSON', function () {
-//  //    var mockResponse = httpMocks.createResponse();
-//  //    controller.list(null, mockResponse, function () {
-//  //      mockResponse._isJSON().should.equal(true, 'Response was not JSON');
-//  //    });
-//  //  });
-//  //
-//  //  it('should respond with success', function () {
-//  //    var mockResponse = httpMocks.createResponse();
-//  //    controller.list(null, mockResponse, function () {
-//  //      mockResponse.statusCode.should.equal(200);
-//  //    });
-//  //  });
-//  //
-//  //  it('should return a JSON array', function () {
-//  //    var mockResponse = httpMocks.createResponse();
-//  //    controller.list(null, mockResponse, function () {
-//  //      var data = JSON.parse(mockResponse._getData());
-//  //      _.isArray(data).should.equal(true, 'Response was not a JSON array');
-//  //    });
-//  //  });
-//  //});
-//  //
-//  //describe('#random', function () {
-//  //
-//  //  it('should respond with JSON', function(done) {
-//  //    var mockResponse = httpMocks.createResponse();
-//  //    controller.random(null, mockResponse, function() {
-//  //      mockResponse._isJSON().should.equal(true);
-//  //      done();
-//  //    });
-//  //  });
-//  //
-//  //  it('should respond with success', function(done) {
-//  //    var mockResponse = httpMocks.createResponse();
-//  //    controller.random(null, mockResponse, function() {
-//  //      mockResponse.statusCode.should.equal(200);
-//  //      done();
-//  //    });
-//  //  });
-//  //
-//  //  it('should return a built experiment', function(done) {
-//  //    var mockResponse = httpMocks.createResponse();
-//  //    controller.random(null, mockResponse, function() {
-//  //      var data = JSON.parse(mockResponse._getData());
-//  //      console.log(data);
-//  //      data.media.length.should.equal(fullExperimentSchema.trialCount);
-//  //      done();
-//  //    });
-//  //  });
-//  //});
-//  //
-//  //after(function () {
-//  //  ExperimentSchema.remove().exec();
-//  //  Media.remove().exec();
-//  //});
-//});
+'use strict';
+
+// Testing tools
+var should = require('chai').should();
+var rewire = require('rewire');
+var sinon = require('sinon');
+
+// Need to make sure the schema has been registered
+require('../../models/experiment-schema');
+
+// Variables for tests
+var controller;
+
+describe('ExperimentSchema Controller Tests', function() {
+
+    beforeEach(function() {
+        controller = rewire('../../controllers/experiment-schema');
+    });
+
+    // TODO: Test routes separately
+    describe('#list', function() {
+
+        it('should use the mongoose model to find all schemas', function() {
+
+            // Spy on calls to ExperimentSchema.find()
+            var spy = sinon.spy();
+            controller.__set__({
+                ExperimentSchema: {
+                    find: spy
+                }
+            });
+
+            // Call the method
+            controller.list();
+
+            // Check expectations
+            spy.callCount.should.equal(1);
+
+            //noinspection JSUnresolvedVariable
+            spy.args[0][0].should.deep.equal({});
+        });
+    });
+
+    describe('#random', function() {
+
+        it('should count the number of schemas in the database', function() {
+
+            // Spy on calls to ExperimentSchema.count()
+            var spy = sinon.spy();
+            controller.__set__({
+                ExperimentSchema: {
+                    count: spy
+                }
+            });
+
+            // Call the method
+            controller.random();
+
+            // Check expectations
+            spy.callCount.should.equal(1);
+
+            //noinspection JSUnresolvedVariable
+            spy.args[0][0].should.deep.equal({});
+        });
+
+        it('should get one schema', function(done) {
+
+            // Mock ExperimentSchema.count() and the chained call to
+            // ExperimentSchema.find()
+            var mockCount = function(filter, callback) {
+                callback(null, 10);
+            };
+
+            controller.__set__({
+                ExperimentSchema: {
+                    count: mockCount,
+                    find: function() {
+                        return {
+                            skip: function(x) {
+
+                                //noinspection JSUnresolvedFunction
+                                x.should.be.below(10);
+                                return {
+                                    limit: function(y) {
+                                        y.should.equal(1);
+                                        done();
+                                        return {
+                                            populate: function() {
+                                                return {
+                                                    exec: function() {
+                                                    }
+                                                };
+                                            }
+                                        };
+                                    }
+                                };
+                            }
+                        };
+                    }
+                }
+            });
+
+            // Call the method
+            controller.random();
+        });
+    });
+});
