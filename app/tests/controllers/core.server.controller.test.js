8b06b10630addc6d196bab5aac52b208703db969
Brennon Bortz <brennon@brennonbortz.com>
2015-09-23 11:28:29 -0400

    Fix tests in core.server.controller.test.js

--- app/tests/controllers/core.server.controller.test.js
+++ app/tests/controllers/core.server.controller.test.js
@@ -4,6 +4,10 @@
 
 'use strict';
 
+// Server
+var server = require('../../../server');
+var app;
+
 // Config module
 var config = require('../../../config/config');
 
@@ -12,31 +16,42 @@ var request = require('supertest');
 var should = require('chai').should();
 
 describe('Core Controller Tests', function() {
-
-  describe('sessions', function() {
-
-    var agent = request.agent('localhost:' + config.port);
-    var cookieA, cookieB;
-
-    it('should be in use', function(done) {
-      agent.get('/').expect('set-cookie', /connect\.sid.*/).end(function(err, res) {
-        if (err) {
-            return done(err);
-        }
-        cookieA = res.header['set-cookie'];
-        done();
-      });
+    before(function(done) {
+        server.then(function(startedApp) {
+            app = startedApp;
+            done();
+        });
     });
 
-    it('should be reset on any visit to the index', function(done) {
-      agent.get('/').expect('set-cookie', /connect\.sid.*/).end(function(err, res) {
-        if (err) {
-            return done(err);
-        }
-        cookieB = res.header['set-cookie'];
-        cookieA.should.not.equal(cookieB);
-        done();
-      });
+    describe('sessions', function() {
+        var agent = request.agent('localhost:' + config.port);
+        var cookieA, cookieB;
+
+        it('should be in use', function(done) {
+            agent
+                .get('/')
+                .expect('set-cookie', /connect\.sid.*/)
+                .end(function(err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+                    cookieA = res.header['set-cookie'];
+                    done();
+                });
+        });
+
+        it('should be reset on any visit to the index', function(done) {
+            agent
+                .get('/')
+                .expect('set-cookie', /connect\.sid.*/)
+                .end(function(err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+                    cookieB = res.header['set-cookie'];
+                    cookieA.should.not.equal(cookieB);
+                    done();
+                });
+        });
     });
-  });
 });
