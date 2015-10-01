418fb78dcc6787fedb2043797368a1115136e856
Brennon Bortz <brennon@brennonbortz.com>
2015-10-01 15:13:58 -0400

    Document LastScreenController

--- public/modules/core/controllers/last-screen.client.controller.js
+++ public/modules/core/controllers/last-screen.client.controller.js
@@ -1,7 +1,22 @@
 'use strict';
 
-angular.module('core').controller('LastScreenController', ['$scope', 'TrialData', '$http',
-    function($scope, TrialData, $http) {
-        $http.post('/api/trials', TrialData.data);
-    }
-]);
+/**
+ * The `LastScreenController` simply posts the object in `TrialData.data` to
+ * the `/api/trials` API endpoint on the server to be written to a file.
+ *
+ * @class Angular.LastScreenController
+ * @see Angular.TrialData
+ * @see Node.module:TrialServerRoutes
+ * @see Node.module:TrialServerController
+ */
+
+angular.module('core').controller(
+    'LastScreenController',
+    [
+        '$scope',
+        'TrialData',
+        '$http',
+        function($scope, TrialData, $http) {
+            $http.post('/api/trials', TrialData.data);
+        }
+    ]);
