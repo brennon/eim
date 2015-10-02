e86f7635e85c7d3d587158ecc79c16b233c40e3d
Brennon Bortz <brennon@brennonbortz.com>
2015-10-02 11:18:19 -0400

    Document displayTrialData directive

--- public/modules/core/directives/display-trial-data.client.directive.js
+++ public/modules/core/directives/display-trial-data.client.directive.js
@@ -1,14 +1,28 @@
 'use strict';
 
 /**
+ * The `displayTrialData` directive is simply a convenient way to display
+ * the current {@link Angular.TrialData#data|TrialData#data} anywhere in the
+ * view.
+ *
+ * @example <display-trial-data></display-trial-data>
+ *
  * @class Angular.displayTrialDataDirective
  */
 
-// Display Trial Data for debugging
-angular.module('core').directive('displayTrialData', function() {
+angular.module('core').directive('displayTrialData', [
+    '$log',
+    function($log) {
 
-    return {
-        restrict: 'AE',
-        template: '<div class="row trial-data-row"><div class="col-md-12"><h3>Trial Data</h3><pre>{{trialDataJson()}}</pre></div></div>'
-    };
-});
+        $log.debug('Rendering displayTrialData directive.');
+
+        return {
+            restrict: 'AE',
+            template: '<div class="row trial-data-row">' +
+            '<div class="col-md-12">' +
+            '<h3>Trial Data</h3>' +
+            '<pre>{{trialDataJson()}}</pre>' +
+            '</div>' +
+            '</div>'
+        };
+    }]);
