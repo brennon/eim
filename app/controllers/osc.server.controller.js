b9edbb9faaa31a9d4614ccfffac38414a453ab13
Brennon Bortz <brennon@brennonbortz.com>
2015-10-14 21:07:27 +0200

    Don't truncate Max log messages
    Fixes #130

--- app/controllers/osc.server.controller.js
+++ app/controllers/osc.server.controller.js
@@ -53,7 +53,7 @@ function buildLogMessageFromMessage(msg) {
 
     var logMessageParts = msg.splice(1);
 
-    var logMessage = '';
+    var logMessage = 'MaxMSP: ';
 
     for (var i = 0; i < logMessageParts.length; i++) {
         logMessage += logMessageParts[i].value;
@@ -63,10 +63,12 @@ function buildLogMessageFromMessage(msg) {
     }
 
     if (level === 'INFO') {
-        console.info('MaxMSP: ' + logMessage);
+        console.info(logMessage);
     } else if (level === 'ERROR') {
-        console.error('MaxMSP: ' + logMessage);
+        console.error(logMessage);
     }
+
+    return logMessage;
 }
 
 // Incoming message handler
@@ -78,22 +80,25 @@ function incomingMessageHandler(msg) {
     if (oscFromBuffer.address === '/eim/status/log') {
 
         // Log the message
-        buildLogMessageFromMessage(oscFromBuffer.args);
+        exports.eventEmitter.emit(
+            'oscMessageReceived',
+            buildLogMessageFromMessage(oscFromBuffer.args)
+        );
+    } else {
 
+        /**
+         * This event is fired when the server receives an OSC message.
+         *
+         * The object sent with this event is a JavaScript object
+         * representation of the OSC message.
+         *
+         * @event oscMessageReceived
+         * @type {{}}
+         */
+
+            // Pass the message on to the socket
+        exports.eventEmitter.emit('oscMessageReceived', oscFromBuffer);
     }
-
-    /**
-     * This event is fired when the server receives an OSC message.
-     *
-     * The object sent with this event is a JavaScript object
-     * representation of the OSC message.
-     *
-     * @event oscMessageReceived
-     * @type {{}}
-     */
-
-    // Pass the message on to the socket
-    exports.eventEmitter.emit('oscMessageReceived', oscFromBuffer);
 }
 
 // Create listener socket
