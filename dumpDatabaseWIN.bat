4bf098a4330c6cef8574ae587c73906a1a2905b5
Brennon Bortz <brennon@brennonbortz.com>
2017-08-12 14:05:19 -0400

    Fix paths in database dump script

--- dumpDatabaseWIN.bat
+++ dumpDatabaseWIN.bat
@@ -1,9 +1,9 @@
 :: Start Emotion in Motion in Windows Machine
 
 :: Imports latest dataBase dump (mongod needs to be running)
-mongodump --port 27017 -d emotion-in-motion-dev --out ./mongodb-dump/emotion-in-motion-dev
-mongodump --port 27017 -d emotion-in-motion-test --out ./mongodb-dump/emotion-in-motion-test
-mongodump --port 27017 -d emotion-in-motion-production --out ./mongodb-dump/emotion-in-motion-production
+mongodump --port 27017 -d emotion-in-motion-dev --out ./mongodb-dump
+mongodump --port 27017 -d emotion-in-motion-test --out ./mongodb-dump
+mongodump --port 27017 -d emotion-in-motion-production --out ./mongodb-dump
 
 :: Start Node (not necessary in EiM terminals)
 :: node server.js
