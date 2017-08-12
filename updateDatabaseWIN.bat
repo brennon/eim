d48daca56e4869aca8434fdfc82c9f8856861e70
Brennon Bortz <brennon@brennonbortz.com>
2017-08-12 13:41:55 -0400

    Change port in database update script

--- updateDatabaseWIN.bat
+++ updateDatabaseWIN.bat
@@ -1,9 +1,9 @@
 :: Start Emotion in Motion in Windows Machine
 
 :: Imports latest dataBase dump (mongod needs to be running)
-mongorestore --port 28017 -d emotion-in-motion-dev --drop ./mongodb-dump/emotion-in-motion-dev
-mongorestore --port 28017 -d emotion-in-motion-test --drop ./mongodb-dump/emotion-in-motion-test
-mongorestore --port 28017 -d emotion-in-motion-production --drop ./mongodb-dump/emotion-in-motion-production
+mongorestore --port 27017 -d emotion-in-motion-dev --drop ./mongodb-dump/emotion-in-motion-dev
+mongorestore --port 27017 -d emotion-in-motion-test --drop ./mongodb-dump/emotion-in-motion-test
+mongorestore --port 27017 -d emotion-in-motion-production --drop ./mongodb-dump/emotion-in-motion-production
 
 :: Start Node (not necessary in EiM terminals)
 :: node server.js
